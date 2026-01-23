4a986f45d60b320f5316980a3495eca58843bc17
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-01-23 22:52:46 +0100

    profile

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\Api\ReserveController;
 
 // Auth routes (publiques)
@@ -16,6 +16,6 @@ Route::apiResource('courses', CourseController::class);
 // Protected routes - authentification requise
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/reservations', ReserveController::class);
-   // Route::apiResource('reserves', ReserveController::class);
+    Route::get('/profile', [UserController::class, 'show']);
     Route::post('/courses/{course}/reserve', [CourseController::class, 'reserveCourse']);
 });
