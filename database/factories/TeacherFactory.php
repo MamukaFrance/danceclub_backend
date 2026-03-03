0ac93e08ca2bae0bda9570674d0ccc5548734b65
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-03-03 20:21:18 +0100

    final fix seed

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -10,6 +10,7 @@ use App\Models\User;
  */
 class TeacherFactory extends Factory
 {
+    protected $model = Teacher::class;
     /**
      * Define the model's default state.
      *
--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -16,16 +16,11 @@ class TeacherSeeder extends Seeder
    {
         // Récupérer tous les editors
         $editors = User::role('editor')->get();
-        // Définir des styles fixes
-        $styles = ['Ballet', 'Hip Hop', 'Salsa', 'Jazz', 'Tap', 'Ballroom'];
 
         foreach ($editors as $index => $editor) {
-            // Créer un teacher pour chaque editor
-            $teacher = Teacher::create([
+            // Créer un teacher pour chaque editor 
+            Teacher::factory()->create([
                 'user_id' => $editor->id,
-                'style' => $styles[$index % count($styles)],
-                'bio' => "Bio par défaut pour le teacher {$editor->name}", // texte fixe
-                'photo' => "https://i.pravatar.cc/400?img=" . ($index + 1), // avatar fixe
             ]);
         }
     }
