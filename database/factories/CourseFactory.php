537fa0e45edd12ddb24eaad9de88568cc43ee347
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-03-03 19:58:30 +0100

    seed course

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -4,12 +4,14 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Teacher;
+use App\Models\Course;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
  */
 class CourseFactory extends Factory
 {
+    protected $model = Course::class;
     /**
      * Define the model's default state.
      *
