6f6c1222fba7fab070a30a4e8df2e1c411d8821d
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-03-27 00:14:22 +0100

    anulation

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Teacher;
 use App\Models\Course;
-use function fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
@@ -24,25 +23,25 @@ class CourseFactory extends Factory
         $teacher = Teacher::inRandomOrder()->firstOrFail();
 
         // Capacité du cours
-        $capacity = fake()->numberBetween(10, 30);
+        $capacity = $this->faker->numberBetween(10, 30);
 
         // Générer un horaire réaliste
-        $startHour = fake()->numberBetween(8, 20); // entre 8h et 20h
-        $startMinute = fake()->randomElement([0, 15, 30, 45]);
+        $startHour = $this->faker->numberBetween(8, 20); // entre 8h et 20h
+        $startMinute = $this->faker->randomElement([0, 15, 30, 45]);
         $start_time = sprintf('%02d:%02d:00', $startHour, $startMinute);
         $end_time = date('H:i:s', strtotime($start_time . ' +1 hour'));
 
         return [
             'teacher_id' => $teacher->id,
-            'title' => fake()->sentence(3),
+            'title' => $this->faker->sentence(3),
             'style' => $teacher->style,
-            'level' => fake()->randomElement(['beginner', 'intermediate', 'advanced']),
+            'level' => $this->faker->randomElement(['beginner', 'intermediate', 'advanced']),
             'capacity' => $capacity,
             'remaining_seats' => $capacity,
-            'date' => fake()->dateTimeBetween('now', '+3 months')->format('Y-m-d'),
+            'date' => $this->faker->dateTimeBetween('now', '+3 months')->format('Y-m-d'),
             'start_time' => $start_time,
             'end_time' => $end_time,
-            'description' => fake()->paragraph(),
+            'description' => $this->faker->paragraph(),
         ];
     }
 }
--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
 use App\Models\Teacher;
-use function fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Teacher>
@@ -21,14 +20,14 @@ class TeacherFactory extends Factory
     public function definition(): array
     {
         // Générer un ID aléatoire pour l'avatar pour que chaque teacher ait une tête différente
-        $avatarId = fake()->numberBetween(1, 70);
+        $avatarId = $this->faker->numberBetween(1, 70);
 
         return [
-            'style' => fake()->randomElement([
+            'style' => $this->faker->randomElement([
                 'Ballet', 'Hip Hop', 'Salsa', 
                 'Jazz', 'Tap', 'Ballroom'
                 ]),
-            'bio' => fake()->paragraph(),
+            'bio' => $this->faker->paragraph(),
             // URL d'avatar réaliste
             'photo' => "https://i.pravatar.cc/400?img={$avatarId}",
         ];
