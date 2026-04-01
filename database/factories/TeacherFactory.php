64a98006d24c28b3eeb9f0853c3a7767ba65c708
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-01 13:40:54 +0200

    sans labels

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -20,14 +20,14 @@ class TeacherFactory extends Factory
     public function definition(): array
     {
         // Générer un ID aléatoire pour l'avatar pour que chaque teacher ait une tête différente
-        $avatarId = fake()->numberBetween(1, 70);
+        $avatarId = $this->faker->numberBetween(1, 70);
 
         return [
-            'style' => fake()->randomElement([
+            'style' => $this->faker->randomElement([
                 'Ballet', 'Hip Hop', 'Salsa', 
                 'Jazz', 'Tap', 'Ballroom'
                 ]),
-            'bio' => fake()->paragraph(),
+            'bio' => $this->faker->paragraph(),
             // URL d'avatar réaliste
             'photo' => "https://i.pravatar.cc/400?img={$avatarId}",
         ];
