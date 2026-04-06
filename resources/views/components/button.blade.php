f9067806470605510205f8ad88d2736f4e224f1f
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-07 00:39:39 +0200

    part3

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,7 +4,14 @@
 ])
 
 @php
-    $classes = 'bg-blue-700 text-white px-3 py-1.5 rounded-md cursor-pointer hover:bg-blue-800 transition font-semibold';      
+    $variants = [
+        'primary' => 'bg-blue-700 hover:bg-blue-800 text-white',
+        'secondary' => 'bg-gray-500 hover:bg-gray-600 text-white',
+        'danger' => 'bg-red-500 hover:bg-red-600 text-white',
+        'reset' => 'bg-yellow-500 hover:bg-yellow-600 text-white',
+    ];
+
+    $classes = ($variants[$variant] ?? $variants['primary']) . ' px-3 py-1.5 rounded-md cursor-pointer transition font-semibold';
 @endphp
 
 @if($href)
