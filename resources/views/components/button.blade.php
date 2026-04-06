ec25d3630afc2ffa30d2cb9bf0a4834d734ac1c0
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-07 00:41:28 +0200

    part4

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -14,15 +14,3 @@
     $classes = ($variants[$variant] ?? $variants['primary']) . ' px-3 py-1.5 rounded-md cursor-pointer transition font-semibold';
 @endphp
 
-@if($href)
-    <a href="{{ $href }}" {{ $attributes->merge(['class' => $classes]) }}>
-        {{ $slot }}
-    </a>
-@else
-    <button {{ $attributes->merge([
-        'type' => 'button',
-        'class' => $classes
-    ]) }}>
-        {{ $slot }}
-    </button>
-@endif
