862172267265012b9cd7d70e1779a5701c4a5879
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-07 00:36:58 +0200

    part2

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -3,3 +3,19 @@
     'variant' => 'primary'
 ])
 
+@php
+    $classes = 'bg-blue-700 text-white px-3 py-1.5 rounded-md cursor-pointer hover:bg-blue-800 transition font-semibold';      
+@endphp
+
+@if($href)
+    <a href="{{ $href }}" {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button {{ $attributes->merge([
+        'type' => 'button',
+        'class' => $classes
+    ]) }}>
+        {{ $slot }}
+    </button>
+@endif
