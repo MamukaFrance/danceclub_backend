0f9e880d1bcf875014c247e0d9297bade7451c0a
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-07 00:53:33 +0200

    part5

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -3,14 +3,17 @@
     'variant' => 'primary'
 ])
 
-@php
-    $variants = [
-        'primary' => 'bg-blue-700 hover:bg-blue-800 text-white',
-        'secondary' => 'bg-gray-500 hover:bg-gray-600 text-white',
-        'danger' => 'bg-red-500 hover:bg-red-600 text-white',
-        'reset' => 'bg-yellow-500 hover:bg-yellow-600 text-white',
-    ];
-
-    $classes = ($variants[$variant] ?? $variants['primary']) . ' px-3 py-1.5 rounded-md cursor-pointer transition font-semibold';
-@endphp
-
+@if($href)
+    <a href="{{ $href }}" {{ $attributes->merge([
+        'class' => 'bg-blue-700 hover:bg-blue-800 text-white px-3 py-1.5 rounded-md cursor-pointer transition font-semibold'
+    ]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button {{ $attributes->merge([
+        'type' => 'button',
+        'class' => 'bg-blue-700 hover:bg-blue-800 text-white px-3 py-1.5 rounded-md cursor-pointer transition font-semibold'
+    ]) }}>
+        {{ $slot }}
+    </button>
+@endif
