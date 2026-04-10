ddbaa345291c356a87ae2ddeec0a3cab30e76774
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-10 13:26:37 +0200

    anullation

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,10 +2,3 @@
     'href' => null,
     'variant' => 'primary'
 ])
-
-<button {{ $attributes->merge([
-        'type' => 'button',
-        'class' => 'bg-blue-700 text-white px-3 py-1.5 rounded-md  cursor-pointer hover:bg-blue-800 transition font-semibold'
-        ]) }}>
-        {{ $slot }}
-</button>
