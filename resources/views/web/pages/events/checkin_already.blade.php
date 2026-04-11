23a4af09ff5bf2c1017727dac6146ae3f32a7bc7
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-11 18:29:29 +0200

    que event8

--- resources/views/web/pages/events/checkin_already.blade.php
+++ resources/views/web/pages/events/checkin_already.blade.php
@@ -4,7 +4,15 @@
 
 @section('content')
     <div class="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-         <div class="w-full max-w-md bg-white rounded shadow-lg p-6 text-center border-2 border-yellow-400">
+         <div class="w-full max-w-md bg-white rounded shadow-lg p-6 text-center">
+
+
+            <!-- Icône -->
+            <div class="flex justify-center mb-4">
+                <div class="bg-yellow-100 text-yellow-600 rounded-full p-4">
+                    ⚠️
+                </div>
+            </div>
             <h2>Ce participant a deja fait son checkin sur l'event</h2> 
         </div>
     </div>
