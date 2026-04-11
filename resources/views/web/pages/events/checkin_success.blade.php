e9e8a862de9d220251b760504cc761ef5f84ee39
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-11 23:37:06 +0200

    succeses1

--- resources/views/web/pages/events/checkin_success.blade.php
+++ resources/views/web/pages/events/checkin_success.blade.php
@@ -9,9 +9,7 @@
             <!-- Icône -->
             <div class="flex justify-center mb-4">
                 <div class="bg-green-100 text-green-600 rounded-full p-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-                    </svg>
+                    ✅
                 </div>
             </div>
 
