70cd94b8d6aa7d4e175e5763cd5cedebdb244e4a
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-11 23:13:30 +0200

    invalid5

--- resources/views/web/pages/events/checkin_invalid.blade.php
+++ resources/views/web/pages/events/checkin_invalid.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-        <div class="w-full max-w-md bg-white rounded-2xl shadow-lg p-6 text-center">
+        <div class="w-full max-w-md bg-white rounded shadow-lg p-6 text-center">
 
             <!-- Icône -->
             <div class="flex justify-center mb-4">
