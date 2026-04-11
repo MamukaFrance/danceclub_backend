ffc66386657ffdc0a9cd78e63403ea7df397ca61
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-11 23:49:00 +0200

    succeses6

--- resources/views/web/pages/events/checkin_success.blade.php
+++ resources/views/web/pages/events/checkin_success.blade.php
@@ -6,7 +6,12 @@
     <div class="min-h-screen flex items-center justify-center bg-gray-100 px-4">
         <div class="w-full max-w-md bg-white rounded shadow-lg p-6 text-center">
 
-            
+            <!-- Icône -->
+            <div class="flex justify-center mb-4">
+                <div class="rounded-full p-4">
+                    ✅
+                </div>
+            </div>
 
             <!-- Titre -->
             <h2 class="text-2xl font-bold text-gray-800 mb-2">
