0a73af730909d7aee5d95bf9a49a1ac617217d99
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-11 23:20:08 +0200

    invalid7

--- resources/views/web/pages/events/checkin_invalid.blade.php
+++ resources/views/web/pages/events/checkin_invalid.blade.php
@@ -29,7 +29,7 @@
             </div>
 
             <!-- Boutons -->
-            <div class="mt-6 flex flex-col sm:flex-row gap-3 justify-center">
+            <div class="mt-6">
                 <a href="{{ route('home') }}"
                 class="bg-gray-800 hover:bg-gray-900 text-white font-semibold py-2 px-5 rounded transition">
                     Accueil
