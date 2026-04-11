fe7aacd30907af5bfd9f2358018e17f9bb80bbd4
Mamuka BAGATELIA <mamukapoti+1@gmail.com>
2026-04-11 19:01:18 +0200

    que event9

--- resources/views/web/pages/events/checkin_already.blade.php
+++ resources/views/web/pages/events/checkin_already.blade.php
@@ -5,11 +5,9 @@
 @section('content')
     <div class="min-h-screen flex items-center justify-center bg-gray-100 px-4">
          <div class="w-full max-w-md bg-white rounded shadow-lg p-6 text-center">
-
-
             <!-- Icône -->
             <div class="flex justify-center mb-4">
-                <div class="bg-yellow-100 text-yellow-600 rounded-full p-4">
+                <div class="bg-gray-100 text-gray-600 rounded-full p-4">
                     ⚠️
                 </div>
             </div>
